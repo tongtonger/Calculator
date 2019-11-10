4f1dfb08a1e97397685ef46869f6395a7e2651ed
Tong <1342490381@qq.com>
2019-11-11 00:27:07 +0800

    三角函数和lg,ln

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.calculator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button btn_0;
+
     private Button btn_1;
     private Button btn_2;
     private Button btn_3;
@@ -26,7 +31,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button btn_multply;//*按钮
     private Button btn_divide;//÷号按钮
     private Button btn_equal;//=按钮
-
+    private Button btn_sin;
+    private Button btn_cos;
+    private Button btn_tan;
+    private Button btn_lg;
+    private Button btn_ln;
+    private Button btn_function;//=按钮
     private EditText editText;
 
     @Override
@@ -34,7 +44,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_0 = (Button) findViewById(R.id.num0);
+        Button btn_0 = (Button) findViewById(R.id.num0);
         btn_1 = (Button) findViewById(R.id.num1);
         btn_2 = (Button) findViewById(R.id.num2);
         btn_3 = (Button) findViewById(R.id.num3);
@@ -47,13 +57,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn_point = (Button) findViewById(R.id.point);
         btn_clear = (Button) findViewById(R.id.clear);
         btn_del = (Button) findViewById(R.id.delete);
-
         btn_add = (Button) findViewById(R.id.add);
         btn_sub = (Button) findViewById(R.id.charjian);
         btn_multply = (Button) findViewById(R.id.cheng);
         btn_divide = (Button) findViewById(R.id.chu);
         btn_equal = (Button) findViewById(R.id.eq);
         editText = (EditText) findViewById(R.id.input);
+        btn_function=(Button) findViewById(R.id.function);
+        btn_sin=(Button) findViewById(R.id.sin);
+        btn_cos=(Button) findViewById(R.id.cos);
+        btn_tan=(Button) findViewById(R.id.tan);
+        btn_lg=(Button) findViewById(R.id.log);
+        btn_ln=(Button) findViewById(R.id.ln);
 
         btn_0.setOnClickListener(this);
         btn_1.setOnClickListener(this);
@@ -73,6 +88,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn_multply.setOnClickListener(this);
         btn_divide.setOnClickListener(this);
         btn_equal.setOnClickListener(this);
+        btn_function.setOnClickListener(this);
+        btn_sin.setOnClickListener(this);
+        btn_cos.setOnClickListener(this);
+        btn_tan.setOnClickListener(this);
+        btn_lg.setOnClickListener(this);
+        btn_ln.setOnClickListener(this);
+
     }
 
     @Override
@@ -94,6 +116,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.charjian:
             case R.id.cheng:
             case R.id.chu:
+            case R.id.cos:
+            case R.id.tan:
+            case R.id.log:
+            case R.id.ln:
                 editText.setText(input + ((Button)v).getText());
                 break;
             case R.id.clear:
@@ -107,14 +133,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.eq:
                 editText.setText(getResult());
                 break;
+            case R.id.function:
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+
         }
     }
 
-    public int getResult() {
+    public String getResult() {
         Expression expression=new Expression();
 
         StringBuffer postfix=expression.toPostfix(editText.getText().toString());//传入中缀表达式并得到后缀表达式
-        int result=expression.ToValue(postfix);//得到计算结果result
+        String result=expression.ToValue(postfix);//得到计算结果result
         return  result;
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.help_item:
+                Toast.makeText(this,"This is help",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.exit_item:
+                Toast.makeText(this,"This is exit",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
     }
 }
